c3b0582ff5894f2df75c281647e5a7ecba46c75f
Matthew Sitton <matthewsitton@gmail.com>
2014-09-30 12:00:52 -0500

    Start imlplementation of the window class.

--- pract2d/core/window.py
+++ pract2d/core/window.py
@@ -1,16 +1,48 @@
 import sdl2 as sdl
 
+def init_video():
+    sdl.SDL_Int(0)
+    sdl.SDL_InitSubSystem(sdl.SDL_INT_VIDEO)
+
 class Window(object):
-	def __init__(self, title, width, height):
-		self.titile = title
-		self.width = width
-		self.height = height
+    def __init__(self, title, width, height, fullscreen):
+        self.title = title
+        self.w = width
+        self.h = height
+        self.x = sdl.SDL_WINDOWPOS_UNDEFINED
+        self.y = sdl.SDL_WINDOWPOS_UNDEFINED
+        self.fullscreen = fullscreen
 
-	def flip(self):
-		pass
+        self.flags = sdl.SDL_WINDOW_OPENGL | sdl.SDL_WINDOW_RESIZABLE
 
-	def make_current(self, context):
-		pass
+        if fullscreen:
+            self.flags |= sdl.SDL_WINDOW_FULLSCREEN
+        else:
+            self.x = sdl.SDL_WINDOWPOS_CENTERED
+            self.y = sdl.SDL_WINDOWPOS_CENTERED
 
-	def destroy(self):
-		pass
+        self.window = sdl.SDL_CreateWindow(self.title, self.x, self.y, self.w, self.h)
+
+
+    def flip(self):
+        sdl.SDL_GL_SwapWindow(self.window)
+
+    def make_current(self, context):
+        if context:
+            window = self.window
+            self.context = context
+
+            context.window = self
+            context = context.context
+        else:
+            if self.context:
+                self.context.window = None
+
+            window = None
+            context = None
+            self.context = None
+
+        sdl.SDL_GL_MakeCurrent(window, context)
+
+    def destroy(self):
+        pass
--- pract2d/game/gamemanager.py
+++ pract2d/game/gamemanager.py
@@ -5,7 +5,7 @@ from pract2d.core import context
 class GameManager(object):
 	def __init__(self):
 		self.events = events.Events()
-		self.window = window.Window('Hello World!', 800, 600)
+		self.window = window.Window('Hello World!', 800, 600, False)
 		self.context = context.Context(3, 3)
 
 	def render(self):
