49b09fc983ca0baa64af6e1b99c45b3bed7ef01d
Matthew Sitton <matthewsitton@gmail.com>
2014-10-01 03:50:55 -0500

    Implement basic event system plus a bit of work on window.py

--- pract2d/core/events.py
+++ pract2d/core/events.py
@@ -1,6 +1,90 @@
-class Events(object):
-	def __init__(self):
-		pass
+import ctypes as ct
+import sdl2 as sdl
+import window
 
-	def run(self):
-		pass
+class EventListenerBase(object):
+    def quit(self):
+        pass
+
+    def key_up(self):
+        pass
+
+    def key_down(self):
+        pass
+
+    def window_close(self):
+        pass
+
+    def window_resize(self):
+
+class Events(object):
+
+    def __init__(self):
+        sdl.SDL_InitSubSystem(sdl.SDL_INIT_EVENTS)
+
+        self.funcListeners = []
+        self.classListeners = []
+
+    def get_listener_list(self, obj):
+        if isinstance(listenerObj, object):
+            return self.classListeners
+        else:
+            return self.funcListeners
+
+    def add_listener(self, listenerObj):
+        listenerList = self.get_listener_list(listenerObj)
+
+        if not listenerObj in listenerList:
+            listenerList.append(listenerObj)
+
+    def remove_listener(self, listenerObj):
+        listenerList = self.get_listener_list(listenerObj)
+
+        if listenerObj in listenerList:
+            listenerList.remove(listenerObj)
+
+    def broadcast_event(self, event, args):
+        for listener in self.classListeners:
+            getattr(listener, event)(*args)
+        for listener in self.funcListeners:
+            listener(event, *args)
+
+    def run(self):
+        event = sdl.SDL_Event()
+
+        while sdl.SDL_PollEvent(ct.byref(event)):
+            # Events with sub-event types
+            # These sub-events can be dispatched instead of the main event.
+            if event.type == sdl.SDL_QUIT:
+                eventName = 'quit'
+                data = tuple()
+
+            elif event.type == sdl.SDL_KEYUP:
+                eventName = 'key_up'
+
+                data = (event.key.keysym.scancode,
+                        event.key.keysym.mod)
+
+            elif event.type == sdl.SDL_KEYDOWN:
+                eventName = 'key_down'
+                data = (event.key.keysym.scancode,
+                        event.key.keysym.sym,
+                        event.key.keysym.mod)
+
+            elif event.type == sdl.SDL_WINDOWEVENT:
+                _winEvent = event.window.event
+                sdlWinID = event.window.windowID
+                winID = window.BWinMan._from_sdl_id(sdlWinID)
+                # Might use later on but for now its just here
+                windowInstance = window.BWinMan._windows[winID]
+
+                if _winEvent == sdl.SDL_WINDOWEVENT_CLOSE:
+                    eventName = 'window_close'
+                    data = tuple(winID)
+
+                elif _winEvent == sdl.SDL_WINDOWEVENT_RESIZE:
+                    eventName = 'window_resize'
+                    data = (winID, event.window.data1, event.window.data2)
+
+            self.broadcast_event(eventName, data)
+            
--- pract2d/core/window.py
+++ pract2d/core/window.py
@@ -4,7 +4,43 @@ def init_video():
     sdl.SDL_Int(0)
     sdl.SDL_InitSubSystem(sdl.SDL_INT_VIDEO)
 
-class Window(object):
+class BWinMan(object):
+    '''
+    This class is a bit of an odd one
+    Its not required, but i thought since what im doing here is a bit differant
+    It might be good to seperate it out in a base class for more clarity.
+
+    Basically what i going on here, this base class contains static values
+    that will contain various values needed for multiple window operation
+    Mainly a list of all window instances, and a dict to translate from sdl window
+    id's to our internal ones. (needed in the events code)
+    '''
+    _windows = []
+    _windowMap = {}
+
+    def register(self):
+        if not instance in BWinMan._windows:
+            winID = len(BWinMan._windows)
+            sldID = sdl.SDL_GetWindowID(self.window)
+
+            BWinMan._windows.append(instance)
+            BWinMan._windowMap[sdlID] = winID
+
+            return winID
+
+    def unregister(self):
+        if instance in BWinMan.windows:
+            for n, item in enumerate(self.window):
+                if item is instance:
+                    # We don't want to remove the item in the list
+                    # Because that would invalidate all other id's
+                    BWinMan.windows[n] = None
+
+    def _from_sdl_id(self, sdlWindowID):
+        return BWinMan._windowMap[sdlWindowID]
+
+
+class Window(BWinMan):
     def __init__(self, title, width, height, fullscreen):
         self.title = title
         self.w = width
@@ -12,6 +48,9 @@ class Window(object):
         self.x = sdl.SDL_WINDOWPOS_UNDEFINED
         self.y = sdl.SDL_WINDOWPOS_UNDEFINED
         self.fullscreen = fullscreen
+
+        # Register with base class
+        self.id = self.register()
 
         self.context = None
 
@@ -48,3 +87,5 @@ class Window(object):
 
     def destroy(self):
         sdl.SDL_DestroyWindow(self.window)
+        # Unregister from base class
+        self.unregister()
