604a3957d90a2f5484d0cd2361f1421cdf2dc31e
Matthew Sitton <matthewsitton@gmail.com>
2014-10-01 04:06:14 -0500

    Start context implementation

--- pract2d/core/context.py
+++ pract2d/core/context.py
@@ -1,7 +1,33 @@
 class Context(object):
-    def __init__(self, major, minor):
+    def __init__(self, major, minor, msaa=2):
         self.major = major
         self.minor = minor
+        self.msaa = msaa
+
+        self.context = None
+        self._window = None
+
+        sdl.SDL_GL_SetAttribute(sdl.SDL_GL_DOUBLEBUFFER, 1)
+
+        sdl.SDL_GL_SetAttribute(sdl.SDL_GL_CONTEXT_MAJOR, major)
+        sdl.SDL_GL_SetAttribute(sdl.SDL_GL_CONTEXT_MINOR, minor)
+
+        sdl.SDL_GL_SetAttribute(sdl.SDL.GL_CONTEXT_PROFILE_MASK, sdl.SDL_GL_CONTEXT_PROFILE_CORE)
+
+        if msaa < 0:
+            sdl.SDL_GL_SetAttribute(sdl.SDL_GL_MULTISAMPLEBUFFERS, 1)
+            sdl.SDL_GL_SetAttribute(sdl.SDL_GL_MULTISAMPLESAMPLES, msaa)
 
     def destroy(self):
-        pass
+        sdl.SDL_GL_DeleteContext(self.context)
+
+    @property
+    def window(self):
+        return self._window
+
+    @window.setter
+    def window(self, win):
+        self._window = win
+        if self.context == None:
+            # Create context if not already created
+            self.context = sdl.SDL_GL_CreateContext(self._window.window)
