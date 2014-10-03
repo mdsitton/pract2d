c11c0ce702c5e5595ed25083f8a737d6d6e94b0c
Matthew Sitton <matthewsitton@gmail.com>
2014-10-03 11:09:34 -0500

    Add a few more file utilities

--- pract2d/core/files.py
+++ pract2d/core/files.py
@@ -13,6 +13,17 @@ def get_path():
 
     return path
 
+def resolve_path(*location):
+    path = get_path()
+    location = (path,) + location
+    location = os.sep.join(location)
+    return location
+
+def resolve_file(location, filename):
+    pathInfo = (location, filename)
+    filePath = os.sep.join(pathInfo)
+    return filePath
+
 def read_file(file):
     with open(file, 'r') as file:
         output = file.read()
