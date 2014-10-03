67f3776e855e61788001c13d52a20dc15bee2aeb
Matthew Sitton <matthewsitton@gmail.com>
2014-10-03 11:03:48 -0500

    Add some file utilities

---
+++ pract2d/core/files.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+def get_path():
+    ''' Get top level path from where the application was lauched. '''
+    # split the path at the file extension
+    splitPath = os.path.realpath(sys.path[0]).split('.')
+
+    if len(splitPath) > 1:
+        path = os.sep.join(splitPath[0].split(os.sep)[:-1])
+    else:
+        path = os.sep.join(splitPath[0].split(os.sep))
+
+    return path
+
+def read_file(file):
+    with open(file, 'r') as file:
+        output = file.read()
+        return output
+
