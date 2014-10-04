e45b3d3a2428d3703260c25b4275359bf6786a37
Matthew Sitton <matthewsitton@gmail.com>
2014-10-03 21:10:03 -0500

    Set the default sdl2 library locations.

--- launcher.py
+++ launcher.py
@@ -1,5 +1,13 @@
 from pract2d.game import gamemanager
+from pract2d.core import files
+from platform import system
+import os
 
 if __name__ == '__main__':
+    try:
+        if system() == 'Windows' or not os.environ["PYSDL2_DLL_PATH"]:
+            os.environ["PYSDL2_DLL_PATH"] = files.get_path()
+    except KeyError:
+        pass
     game = gamemanager.GameManager()
     game.run()
